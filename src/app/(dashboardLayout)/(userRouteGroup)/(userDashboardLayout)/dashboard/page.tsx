e728b01f47a600217b9308b81a7b670af3e67963
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2026-03-17 23:37:26 +0600

    refactor: add user authentication check with redirect in dashboard components

--- src/app/(dashboardLayout)/(userRouteGroup)/(userDashboardLayout)/dashboard/page.tsx
+++ src/app/(dashboardLayout)/(userRouteGroup)/(userDashboardLayout)/dashboard/page.tsx
@@ -1,8 +1,13 @@
 import UserDashboardContent from "@/components/modules/Dashboard/UserDashboardContent";
 import { getUserInfo } from "@/services/auth.services";
+import { redirect } from "next/navigation";
 
 const UserDashboardPage = async () => {
     const userInfo = await getUserInfo();
+
+    if (!userInfo) {
+        redirect("/login");
+    }
 
     return <UserDashboardContent userInfo={userInfo} />;
 };
