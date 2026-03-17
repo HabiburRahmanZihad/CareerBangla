a433d4a8477e80980fd9edcfc28801f827e7958f
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2026-03-17 15:12:47 +0600

    feat: enhance CommonLayout with Navbar and Footer components

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,11 +1,19 @@
-export default function CommonLayout({
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
+import { getUserInfo } from "@/services/auth.services";
+
+export default async function CommonLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await getUserInfo();
+
   return (
-   <>
-   {children}
-   </>
+    <div className="flex flex-col min-h-screen">
+      <Navbar user={user} />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
   );
 }
