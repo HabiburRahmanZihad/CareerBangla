1fd22736fd0591ba3f840866a7f078b73a1f01b6
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2026-03-25 22:29:03 +0600

    refactor: remove wallet-related components and services

--- src/app/(dashboardLayout)/(userRouteGroup)/(userDashboardLayout)/dashboard/wallet/page.tsx
+++
@@ -1,9 +0,0 @@
-import WalletContent from "@/components/modules/Dashboard/WalletContent";
-import { protectPageByRole } from "@/lib/protectedPageHelpers";
-
-const WalletPage = async () => {
-    await protectPageByRole("USER");
-    return <WalletContent />;
-};
-
-export default WalletPage;
--- src/app/(dashboardLayout)/recruiter/dashboard/wallet/page.tsx
+++
@@ -1,9 +0,0 @@
-import WalletContent from "@/components/modules/Dashboard/WalletContent";
-import { protectPageByRole } from "@/lib/protectedPageHelpers";
-
-const RecruiterWalletPage = async () => {
-    await protectPageByRole("RECRUITER");
-    return <WalletContent />;
-};
-
-export default RecruiterWalletPage;
