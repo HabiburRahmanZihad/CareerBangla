32cac1a0cd9303d79395875b115501f8de8fc7b8
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2026-03-25 11:20:30 +0600

    fix: format code for consistency in DashboardNavbar component

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -6,17 +6,17 @@ import { redirect } from "next/navigation"
 import DashboardNavbarContent from "./DashboardNavbarContent"
 
 const DashboardNavbar = async () => {
-   const userInfo = await getUserInfo()
+  const userInfo = await getUserInfo()
 
-   if (!userInfo) {
-     redirect("/login")
-   }
+  if (!userInfo) {
+    redirect("/login")
+  }
 
-    const navItems : NavSection[] = getNavItemsByRole(userInfo.role)
-  
-    const dashboardHome = getDefaultDashboardRoute(userInfo.role)
+  const navItems: NavSection[] = getNavItemsByRole(userInfo.role)
+
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role)
   return (
-    <DashboardNavbarContent userInfo={userInfo} navItems={navItems} dashboardHome={dashboardHome}/>
+    <DashboardNavbarContent userInfo={userInfo} navItems={navItems} dashboardHome={dashboardHome} />
   )
 }
 
