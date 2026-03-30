db1afd17325ef6319cc28d0b2207ecd416748e54
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2026-03-31 02:18:26 +0600

    feat: update font settings to use Comic Relief for improved typography consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import { Toaster } from "@/components/ui/sonner";
 import QueryProviders from "@/providers/QueryProvider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Comic_Relief, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const comicRelief = Comic_Relief({
+  variable: "--font-comic-relief",
   subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
@@ -27,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${comicRelief.variable} ${geistMono.variable} font-sans antialiased`}
         suppressHydrationWarning
       >
         <QueryProviders>
